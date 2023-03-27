d079e9679cf927cca44332e890ddbb600b3c33d0
SIDORYCH <vladimiriv@hotmail.com>
2023-03-28 00:44:00 +0300

    Первый коммит

---
+++ src/main/java/model/User.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private String login;
+    private String password;
+
+    private List<Role> rolesList;
+
+}
